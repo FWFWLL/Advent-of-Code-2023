702d4fc265f84259322018ed9abd76a61b3503a3
FWFWLL <ffl@vivaldi.net>
2023-11-30 17:08:27 +0100

    Prepare for Advent of Code 2023

---
+++ day-00/src/bin/part_1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let input = include_str!("../../input/part_1.txt");
+    let output = day_00::part_1::process(input);
+
+    println!("{}", output);
+}
---
+++ day-00/src/part_2.rs
@@ -0,0 +1,3 @@
+pub fn process(input: &str) -> String {
+    input.to_string()
+}
